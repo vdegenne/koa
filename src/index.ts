427ece5a54ceffcb68288aca75b9a5a337c84756
Valentin Degenne <vdegenne@gmail.com>
2025-07-07 21:11:53 +0200

    0.1.4

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
+import cors from '@koa/cors';
 import Router from '@koa/router';
-import type {Context, Next} from 'koa';
+import type {Context, Middleware, Next} from 'koa';
 import Koa from 'koa';
 import serve from 'koa-static';
-import cors from '@koa/cors';
 
 // Extend the Koa Request interface to include a 'body' property
 declare module 'koa' {
@@ -47,4 +47,47 @@ function bodyParser() {
 	};
 }
 
-export {Koa, Router, serve, bodyParser, cors};
+export {bodyParser, cors, Koa, Router, serve};
+
+interface VKoaOptions {
+	port: number;
+
+	get?: Array<[string, ...Router.Middleware[]]>;
+	post?: Array<[string, ...Router.Middleware[]]>;
+
+	middlewares?: Middleware[];
+
+	onStart?: () => void;
+}
+
+export function config(options: VKoaOptions) {
+	const app = new Koa();
+	const router = new Router();
+
+	app.use(bodyParser());
+
+	if (options.middlewares) {
+		for (const middleware of options.middlewares) {
+			app.use(middleware);
+		}
+	}
+
+	if (options.get) {
+		for (const route of options.get) {
+			router.get(...route);
+		}
+	}
+
+	if (options.post) {
+		for (const route of options.post) {
+			router.post(...route);
+		}
+	}
+
+	app.use(router.routes()).use(router.allowedMethods());
+
+	app.listen(options.port, () => {
+		console.log(`Server listening on http://localhost:${options.port}`);
+		if (options.onStart) options.onStart();
+	});
+}
