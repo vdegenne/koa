02352d8e0eb71322f8de24e420a5f0e56cd7f222
Valentin Degenne <vdegenne@gmail.com>
2026-05-11 19:09:42 +0200

    0.1.47

--- src/middlewares/bodyParser.ts
+++ src/middlewares/bodyParser.ts
@@ -8,17 +8,26 @@ export function bodyParser() {
 		) {
 			const data = await new Promise<string>((resolve, reject) => {
 				let raw = '';
+
 				ctx.req.on('data', (chunk) => (raw += chunk));
 				ctx.req.on('end', () => resolve(raw));
 				ctx.req.on('error', (err) => reject(err));
 			});
 
+			const trimmed = data.trim();
+
+			if (trimmed.length === 0) {
+				(ctx.request as any).body = undefined;
+				return next();
+			}
+
 			try {
-				(ctx.request as any).body = JSON.parse(data);
+				(ctx.request as any).body = JSON.parse(trimmed);
 			} catch {
 				ctx.throw(400, 'Invalid JSON');
 			}
 		}
+
 		await next();
 	};
 }
